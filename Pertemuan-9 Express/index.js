57088c05599a359e03d3b552a23c2633f6cf4ee2
Youmingwen <owen.535180016@stu.untar.ac.id>
2020-11-05 20:28:51 +0700

    Bab 9
    Done sampai ke Postman

---
+++ Pertemuan-9 Express/index.js
@@ -0,0 +1,24 @@
+var express = require('express')
+const { allowedNodeEnvironmentFlags } = require('process')
+
+var app = express()
+
+app.get('/', function(req, res){res.send('Hello World!')})
+
+app.post('/', function(req, res){res.send('Ini POST Request!')})
+
+app.put('/user', function(req, res){res.send('PUT Request dijalankan!')})
+
+app.delete('/user', function(req, res){res.send('DELETE Request pada suatu user!')})
+
+app.use(express.static('public'));
+
+//specified port
+app.listen(3000)
+
+/*
+Install Nodemon:
+$ npm install -g nodemon
+$ Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
+$ nodemon
+*/
