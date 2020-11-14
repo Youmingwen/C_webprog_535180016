74ab0b43ac563cb74fe01fd19ff2d58529bdbcba
Youmingwen <owen.535180016@stu.untar.ac.id>
2020-11-14 12:12:52 +0700

    eslint
    eslint

--- Pertemuan-9 NodeJS Express/server.js
+++ Pertemuan-9 NodeJS Express/server.js
@@ -1,12 +1,17 @@
 const { response } = require('express');
 const express = require('express');
 
+const path = require('path');
+
 const app = express();
 
 const port = 3000;
 
+app.use(express.static(path.join(__dirname, './static')));
+
 app.get('/',(request, response) => {
-    response.send("Hello World Express");
+    //response.send("Hello World Express");
+    response.sendFile(path1.join(__dirname, './static/index.html'));
 });
 
 app.listen(port, () => {
