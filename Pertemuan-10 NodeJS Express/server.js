1c98b420cb778788a99ab5a37c1ec76b6234b3ac
Youmingwen <owen.535180016@stu.untar.ac.id>
2020-11-21 02:47:35 +0700

    eslint
    install eslint

--- Pertemuan-10 NodeJS Express/server.js
+++ Pertemuan-10 NodeJS Express/server.js
@@ -1,12 +1,17 @@
 const { request, response } = require('express');
 const express = require('express');
 
+const path = require('path');
+
 const app = express();
 
 const port = 3000;
 
+app.use(express.static(path.join(__dirname,'./static')));
+
 app.get('/',(request, response) => {
-    response.send('hello world express');
+    //response.send('hello world express');
+    response.sendFile(path1.join(__dirname,'./static/index.html'));
 });
 
 app.listen(port,()=>{
